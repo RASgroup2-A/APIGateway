0e5a8c8947de5c3ce1c3a04630d76c2d3d829dd4
joaocasr <castrojoao839@gmail.com>
2023-12-23 01:29:58 +0000

    .

--- services/GestaoNotificacoes.js
+++ services/GestaoNotificacoes.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-module.exports.gestaoNotificacoesAccessPoint = process.env.GESTAO_PROVAS_AP || 'http://localhost:8888';
+module.exports.gestaoNotificacoesAccessPoint = 'http://localhost:8888';
 module.exports.gestaoNotificacoesRoute = (route) => this.gestaoNotificacoesAccessPoint + route
 
 module.exports.getnotifications = (id) => {
